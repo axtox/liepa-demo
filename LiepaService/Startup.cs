6f50d6fbe1afa5475ffdc805f94a9f7f5fdc190b
Anton Kulikov <axtoox@protonmail.com>
2020-11-04 05:08:49 +0300

    Service project created

---
+++ LiepaService/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using LiepaService.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace LiepaService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {            
+            services.AddDbContext<LiepaDemoDatabaseContext>(opt =>
+               opt.UseMySql(Configuration.GetConnectionString("DefaultConnection"), mysqlOptions =>      
+                        mysqlOptions.ServerVersion(new ServerVersion(new Version(10, 1, 37), ServerType.MariaDb))));
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
